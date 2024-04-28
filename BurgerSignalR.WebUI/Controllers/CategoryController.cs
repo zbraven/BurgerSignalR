3825a91b81e48d95c620113fbed01c9ebd448d45
Zahid Buğra Sarıcan <115879495+zbraven@users.noreply.github.com>
2024-04-28 11:14:19 +0300

    Kategoriye ait consume işlemlerini yaptım. WebUI kısmını API kısmıyla eşledim.

--- BurgerSignalR.WebUI/Controllers/CategoryController.cs
+++ BurgerSignalR.WebUI/Controllers/CategoryController.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using BurgerSignalR.WebUI.Dtos.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace BurgerSignalR.WebUI.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44319/api/Category"); //Api'nin hangisinden gelecek talebi seçeceğime karar verdim.
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();  //Gelen içeriği string formatında oku.
+                var  values= JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);  //Json datasını çözüp normal metine dönüştürüyorum.(Listelerken Deserialize, güncellerken-eklerken Serialize)
+                return View(values);
+            }
+
             return View();
         }
     }
